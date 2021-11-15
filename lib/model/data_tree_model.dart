ab7e81856189857821acaa77458663736bc35382
humanghosts <542896234@qq.com>
2021-11-15 17:10:46 +0800

    update

--- lib/model/data_tree_model.dart
+++ lib/model/data_tree_model.dart
@@ -2,23 +2,24 @@ import '../attribute/export.dart';
 import '../util/export.dart';
 import 'data_model.dart';
 
-abstract class DataTreeModel<T> extends DataModel {
+/// T must is a DataTreeModel
+abstract class DataTreeModel<T extends DataModel> extends DataModel {
   late final Attribute<String> path;
   late final Attribute<String> fullPath;
-  late final Attribute<DataTreeModel?> parent;
-  late final ListAttribute<DataTreeModel> children;
+  late final Attribute<T?> parent;
+  late final ListAttribute<T> children;
 
-  final Map<String, DataTreeModel> childrenMap = {};
+  final Map<String, T> childrenMap = {};
 
   DataTreeModel() {
     path = attributes.string(name: "path", title: "路径", dvalue: PathUtils.genPath(length: pathLength));
     fullPath = attributes.string(name: "full_path", title: "绝对路径", dvalue: path.value);
-    parent = attributes.dataTreeModelNullable(
+    parent = attributes.dataModelNullable<T?>(
       name: "parent",
       title: "上级",
       listener: AttributeListener(beforeSetValue: beforeSetParent, afterSetValue: afterSetParent),
     );
-    children = attributes.dataTreeModelList(
+    children = attributes.dataModelList<T>(
       name: "children",
       title: "下级",
       listener: ListAttributeListener(
@@ -32,18 +33,18 @@ abstract class DataTreeModel<T> extends DataModel {
 
   int get pathLength => 4;
 
-  DataTreeModel? getChild(String? id) {
+  T? getChild(String? id) {
     return childrenMap[id];
   }
 
   /// 处理设置parent前 数据合法性校验，以及清空原本上级的中的自己
-  bool beforeSetParent(DataTreeModel? parentValue) {
+  bool beforeSetParent(T? parentValue) {
     // 处理重复设置Parent情况(两个都为null的情况也包含在内)
     if (parent.value == parentValue) {
       return false;
     }
     // 处理有一个为null的情况
-    DataTreeModel? thisParentValue = parent.value;
+    DataTreeModel? thisParentValue = parent.value as DataTreeModel?;
     if (parentValue == null || thisParentValue == null) {
       // parentValue==null thisParenValue!=null
       // 清空parent
@@ -58,7 +59,7 @@ abstract class DataTreeModel<T> extends DataModel {
     if (parentValue.id.value == thisParentValue.id.value) {
       return true;
     }
-    String parentPath = parentValue.path.value;
+    String parentPath = (parentValue as DataTreeModel).path.value;
     // 判断是否是否循环树
     bool isLoop = fullPath.value.contains(parentPath);
     if (isLoop) {
@@ -68,22 +69,22 @@ abstract class DataTreeModel<T> extends DataModel {
   }
 
   /// 设置上级之后设置绝对路径
-  void afterSetParent(DataTreeModel? parentValue) {
+  void afterSetParent(T? parentValue) {
     if (parentValue == null) {
       fullPath.value = path.value;
     } else {
-      fullPath.value = "${parentValue.fullPath.value}|${path.value}";
+      fullPath.value = "${(parentValue as DataTreeModel).fullPath.value}|${path.value}";
       parentValue.children.append(this);
     }
   }
 
-  bool beforeAddChild(int index, DataTreeModel child) {
+  bool beforeAddChild(int index, T child) {
     if (childrenMap.containsKey(child.id.value)) {
       return false;
     }
     // 这里会出发一次child的setParent，在beforeSetParent里面会进行校验
     // 在afterSetParent中会再次回到这里，然后再次进去beforeSetParent，然后判断上级一样，结束循环
-    child.parent.value = this;
+    (child as DataTreeModel).parent.value = this;
     // 再次判断，因为这个代码会走两次，防止重复添加
     if (childrenMap.containsKey(child.id.value)) {
       return false;
@@ -91,21 +92,21 @@ abstract class DataTreeModel<T> extends DataModel {
     return true;
   }
 
-  void afterAddChild(int index, DataTreeModel child) {
+  void afterAddChild(int index, T child) {
     childrenMap[child.id.value] = child;
   }
 
-  bool beforeRemoveChild(DataTreeModel child) {
+  bool beforeRemoveChild(T child) {
     if (!childrenMap.containsKey(child.id.value)) {
       return false;
     }
     return true;
   }
 
-  void afterRemoveChild(DataTreeModel child) {
+  void afterRemoveChild(T child) {
     childrenMap.remove(child.id.value);
     // 这里会出发一次child的setParent，在beforeSetParent里面会进行校验
     // 会再次回到beforeRemoveChild,判断map里面已经没有了，就会结束循环
-    child.parent.value = null;
+    (child as DataTreeModel).parent.value = null;
   }
 }
