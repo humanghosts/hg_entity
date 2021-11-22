50a79eeb89a028304396e061cd67b6850844ab3a
humanghosts <542896234@qq.com>
2021-11-22 16:16:38 +0800

    update derive model

--- lib/model/derive_model.dart
+++ lib/model/derive_model.dart
@@ -1,8 +1,11 @@
 import 'package:hg_entity/attribute/attribute.dart';
 
 abstract class DeriveModel {
-  Attribute get originAttribute;
-  List<Attribute> get deriveAttributes;
+  /// 关联原始模型的字段，derive.origin.value == origin.derive.value
+  Attribute<String> get originAttribute;
+
+  /// 关联派生模型的字段
+  Attribute<String> get deriveAttributes;
 
   List<DeriveModel> derive({int num = 1});
 }
