a284d92a0967b81473e605e9fff524b5ec7040f3
humanghosts <542896234@qq.com>
2021-11-22 16:18:43 +0800

    update derive model

--- lib/model/derive_model.dart
+++ lib/model/derive_model.dart
@@ -5,7 +5,7 @@ abstract class DeriveModel {
   Attribute<String> get originAttribute;
 
   /// 关联派生模型的字段
-  Attribute<String> get deriveAttributes;
+  Attribute<String> get deriveAttribute;
 
   List<DeriveModel> derive({int num = 1});
 }
